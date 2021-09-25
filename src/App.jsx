b8ebdfe1c2f1796c6755f02474a303a0e5c95f88
Takumikimura141899 <kimura141899@gmail.com>
2021-09-25 15:05:41 +0900

    [add] App

--- src/App.jsx
+++ src/App.jsx
@@ -4,82 +4,49 @@ import Popup from 'reactjs-popup';
 import SignaturePad from 'react-signature-canvas';
 import './App.css';
 
-function App() {
-  const [imageURL, setImageURL] = useState(null); // create a state that will contain our image url
-  const sigCanvas = useRef({});
-  /* a function
-        that uses the canvas ref to clear the canvas via a method given by
-        react-signature-canvas */
-  const clear = () => sigCanvas.current.clear();
-  /* a function that uses the canvas ref to trim the canvas from white
-        spaces via a method given by react-signature-canvas then saves it in our
-        state */
-  const save = () =>
-    setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL('image/png'));
+const App = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const [localStorageContents, setLocalStorageContents] = useState([]);
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!text) return;
+
+    const newTodo = {
+      value: text,
+      id: new Date().getTime(),
+    };
+
+    const newTodos = [newTodo, ...localStorageContents];
+    // console.log(todos);
+    setText('');
+    localStorage.setItem('contents', JSON.stringify(newTodos));
+    // console.log(localStorage.getItem('contents'));
+    console.log(localStorageContents);
+    const getLocalStorageContents = localStorage.getItem('contents');
+    setLocalStorageContents(JSON.parse(getLocalStorageContents));
+  };
 
   return (
-    <div className="App">
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Hello World</h1>
-          <p className="subtitle">
-            My first website with
-            <strong>Bulma</strong>
-          </p>
-        </div>
-      </section>
-      <div>
-        <button type="button" className="button is-primary is-medium">
-          button
-        </button>
-        <button className="button is-success is-large" type="submit">
-          button
-        </button>
-        <button className="button is-warning is-small" type="reset">
-          button
-        </button>
-      </div>
-      <div className="container">
-        <h1>Signature Pad Example</h1>
-        <Popup
-          modal
-          trigger={<button>Open Signature Pad</button>}
-          closeOnDocumentClick={false}
-        >
-          {(close) => (
-            <>
-              <SignaturePad
-                ref={sigCanvas}
-                canvasProps={{
-                  className: 'signature',
-                }}
-              />
-              {/* Button to trigger save canvas image */}
-              <button onClick={save}>Save</button>
-              <button onClick={clear}>Clear</button>
-              <button onClick={close}>Close</button>
-            </>
-          )}
-        </Popup>
-        <br />
-        <br />
-        {/* if our we have a non-null image url we should
-      show an image and pass our imageURL state to it */}
-      </div>
-      {imageURL ? (
-        <img
-          src={imageURL}
-          alt="my signature"
-          style={{
-            display: 'block',
-            margin: '0 auto',
-            border: '1px solid black',
-            width: '150px',
-          }}
-        />
-      ) : null}
+    <div>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
+        <ul>
+          {localStorageContents.map((todo) => {
+            return <li key={todo.id}>{todo.value}</li>;
+          })}
+        </ul>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
