def8ef3f198437375ea375892e2d9c4e3479f2ad
asuparabaco0 <rina2000428@yahoo.co.jp>
2021-09-25 22:16:11 +0900

    modify someparts

--- src/App.jsx
+++ src/App.jsx
@@ -33,40 +33,39 @@ const App = () => {
     setLocalStorageContents(JSON.parse(getLocalStorageContents));
     setText('');
   };
-
+    const clear=()=>{
+      console.log("aaa")
+      localStorage.clear();
+      console.log("bbb")
+    }
   return (
-    <div className="App mx-6">
+    <div className="App mx-6 ">
       <section className="section">
-        <div className="container m-auto mx-5">
-          <h1 className="title ">あなたの気持ちに、<strong>チャッカマン。</strong></h1>
+        <div className="container is-flex is-justify-content-center m-auto mx-5">
+          <h1 className="title has-text-weight-light is-family-primary">あなたの心に、チャッカマン。</h1>
         </div>
       </section>
-      <section>
-        {/* <i><FontAwesomeIcon icon="fa-solid fa-1"/>aaaaaa</i> */}
-        <section className="section box has-background-grey-dark box-radius-3">
-
-          <form action="submit" onSubmit={handleSubmit} className="field columns is-grouped is-grouped-centered">
-            <input type="text" value={text} onChange={onChange} placeholder="あなたの愚痴をお聞かせください"className="column ml-6 "/>
-            <button type="submit" value="追加" onSubmit={(e) => e.preventDefault()}  type="submit" className="ml-3 mr-5 button is-success is-outlined is-outlined is-medium is-rounded">投稿</button>
+        <div className="form-size section box has-background-grey-dark is-flex is-justify-content-center m-auto">
+          <form action="submit" onSubmit={handleSubmit} className="field columns is-flex is-vcentered">
+            <input type="text" value={text} onChange={onChange} placeholder="愚痴を、どうぞ。"className="input column ml-6 is-success"/>
+            <button value="追加" onSubmit={(e) => e.preventDefault()}  type="submit" className="postBtn ml-3 mr-5 button is-success is-outlined is-normal is-rounded">追加</button>
           </form>
-
-        </section>
-      </section>
+        </div>
       <section className="section">
         <div className="posts">
-          <div className="columns text is-grouped is-flex is-justify-content-center is-vcentered is-centered">
+          <div className="columns text is-grouped is-flex is-flex-wrap-wrap is-flex-direction-column is-align-items-center is-centered">
             {localStorageContents.map((todo) => {
-              return <p className="box column is-5 ml-6 has-text-centered mb-2 mr-1" key={todo.id}>{todo.value}</p>;
+              return <p className="box column is-5 has-text-centered mb-2 is-full" key={todo.id}>{todo.value}</p>;
             })}
           </div>
         </div>
       </section>
-
-      <section className="section">
-        <div className="bomb is-grouped is-grouped-centered">
-          <button className="bomb button is-danger is-clickable is-rounded">
+      <section className="fixed section">
+        <div className="columns is-flex is-flex-direction-column is-justify-content-center ">
+          <button className="is-danger is-outlined column is-medium button">
             爆破
           </button>
+          <button onClick={clear} className="mt-2 clear button column is-medium is-primary is-outlined">RESET</button>
         </div>
       </section>
     </div>
