c29f82e8cca5c40bcc95efc083be03ce5e9d83b4
su-min484 <85155408+su-min484@users.noreply.github.com>
2021-09-25 13:12:10 +0900

    セクションわけ

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,42 @@ const App=()=> {
   return (
     <div className="App">
       <section className="section">
-        <section className="container">
+        <div className="container">
           <h1 className="title">ペーパーレス爆破</h1>
           <p className="subtitle">
             <strong>CO2</strong>も出しません。
           </p>
-        </section>
+        </div>
       </section>
-      <section className="field columns is-grouped is-grouped-centerd">
-        <p className="form-label">不満</p>
-        <input className="column"/>
-        <button type="button" className="button is-primary is-medium">
-          投稿
-        </button>
+
+      <section className="section">
+        <div className="field columns is-grouped is-grouped-centerd">
+          <p className="form-label">不満</p>
+          <input className="column"/>
+          <button type="button" className="button is-primary is-medium">
+            投稿
+          </button>
+        </div>
+
       </section>
-      <section className="posts">
-      <div className="text is-grouped is-centerd">
-        <p>sample</p>
-        <p>sample</p>
-      </div>
+
+      <section className="section">
+        <div className="posts">
+          <div className="text is-grouped is-centerd">
+            <p>sample</p>
+            <p>sample</p>
+          </div>
+        </div>
+
       </section>
-      <section className="bomb is-grouped is-grouped-centerd">
-      <button className="button is-danger">
-        爆破
-      </button>
+
+      <section className="section">
+        <div className="bomb is-grouped is-grouped-centerd">
+          <button className="button is-danger">
+            爆破
+          </button>
+        </div>
+
       </section>
     </div>
   );
