91c22f98c20df5abdaddb7e33d60ecaa77147c3f
Takumikimura141899 <kimura141899@gmail.com>
2021-09-25 17:38:18 +0900

    [clean] PostComplainコンポーネント化

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import { useState, useEffect } from 'react';
 import 'bulma/css/bulma.min.css';
 
 import './App.css';
+import PostComplain from './components/organisms/PostComplain';
 
 const App = () => {
-  const [text, setText] = useState('');
-  const [localStorageContents, setLocalStorageContents] = useState([]);
-
-  useEffect(() => {
-    if (localStorage.getItem('contents')) {
-      const getLocalStorageContents = localStorage.getItem('contents');
-      setLocalStorageContents(JSON.parse(getLocalStorageContents));
-    }
-  }, []);
-
-  const onChange = (e) => {
-    setText(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!text) return;
-
-    const newTodo = {
-      value: text,
-      id: new Date().getTime(),
-    };
-
-    const newTodos = [newTodo, ...localStorageContents];
-    localStorage.setItem('contents', JSON.stringify(newTodos));
-    const getLocalStorageContents = localStorage.getItem('contents');
-    setLocalStorageContents(JSON.parse(getLocalStorageContents));
-    setText('');
-  };
-
   return (
     <>
       <div className="App">
@@ -71,17 +41,7 @@ const App = () => {
           </div>
         </section>
       </div>
-      <div>
-        <form action="submit" onSubmit={handleSubmit}>
-          <input type="text" value={text} onChange={onChange} />
-          <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
-          <ul>
-            {localStorageContents.map((todo) => {
-              return <li key={todo.id}>{todo.value}</li>;
-            })}
-          </ul>
-        </form>
-      </div>
+      <PostComplain />
     </>
   );
 };
---
+++ src/components/molecules/Title.jsx
@@ -0,0 +1,14 @@
+const Title = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">ペーパーレス爆破</h1>
+        <p className="subtitle">
+          <strong>CO2</strong>も出しません。
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Title;
