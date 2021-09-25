f9b86325c679279fe9036c47089017f13f8630c1
RYOTAN9688 <ryota8813@gmail.com>
2021-09-25 15:49:08 +0900

    complete　explosionPage

--- src/routing/routing.js
+++ src/routing/routing.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from '../App';
-import { explosionPage } from '../explosionPage';
+import { explosionPage } from '../exposionPage/explosionPage';
 
 function Routing() {
   return (
