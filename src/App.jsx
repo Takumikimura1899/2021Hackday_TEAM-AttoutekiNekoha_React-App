0bbe43b9d8a9a987c4014ea20f222eaffce2933e
asuparabaco0 <rina2000428@yahoo.co.jp>
2021-09-25 13:08:37 +0900

    added sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,31 @@ const App=()=> {
   return (
     <div className="App">
       <section className="section">
-        <div className="container">
+        <section className="container">
           <h1 className="title">ペーパーレス爆破</h1>
           <p className="subtitle">
             <strong>CO2</strong>も出しません。
           </p>
-        </div>
+        </section>
       </section>
-      <div className="field columns is-grouped is-grouped-centerd">
+      <section className="field columns is-grouped is-grouped-centerd">
         <p className="form-label">不満</p>
         <input className="column"/>
         <button type="button" className="button is-primary is-medium">
           投稿
         </button>
-      </div>
-      <div className="posts is-grouped is-centerd">
+      </section>
+      <section className="posts">
+      <div className="text is-grouped is-centerd">
         <p>sample</p>
         <p>sample</p>
       </div>
-      <div className="bomb is-grouped is-grouped-centerd">
+      </section>
+      <section className="bomb is-grouped is-grouped-centerd">
       <button className="button is-danger">
         爆破
       </button>
-      </div>
+      </section>
     </div>
   );
   }
