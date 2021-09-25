7fe106145a1b0fd864a7d5e72994ff9bc2c6ef52
RYOTAN9688 <ryota8813@gmail.com>
2021-09-25 18:16:35 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -38,25 +38,50 @@ const App = () => {
     <div className="App mx-6">
       <section className="section">
         <div className="container m-auto mx-5">
-          <h1 className="title ">あなたの気持ちに、<strong>チャッカマン。</strong></h1>
+          <h1 className="title ">
+            あなたの気持ちに、<strong>チャッカマン。</strong>
+          </h1>
         </div>
       </section>
       <section>
         {/* <i><FontAwesomeIcon icon="fa-solid fa-1"/>aaaaaa</i> */}
         <section className="section box has-background-grey-dark box-radius-3">
-
-          <form action="submit" onSubmit={handleSubmit} className="field columns is-grouped is-grouped-centered">
-            <input type="text" value={text} onChange={onChange} placeholder="あなたの愚痴をお聞かせください"className="column ml-6 "/>
-            <button type="submit" value="追加" onSubmit={(e) => e.preventDefault()}  type="submit" className="ml-3 mr-5 button is-success is-outlined is-outlined is-medium is-rounded">投稿</button>
+          <form
+            action="submit"
+            onSubmit={handleSubmit}
+            className="field columns is-grouped is-grouped-centered"
+          >
+            <input
+              type="text"
+              value={text}
+              onChange={onChange}
+              placeholder="あなたの愚痴をお聞かせください"
+              className="column ml-6 "
+            />
+            <button
+              type="submit"
+              value="追加"
+              onSubmit={(e) => e.preventDefault()}
+              type="submit"
+              className="ml-3 mr-5 button is-success is-outlined is-outlined is-medium is-rounded"
+            >
+              投稿
+            </button>
           </form>
-
         </section>
       </section>
       <section className="section">
         <div className="posts">
           <div className="columns text is-grouped is-flex is-justify-content-center is-vcentered is-centered">
             {localStorageContents.map((todo) => {
-              return <p className="box column is-5 ml-6 has-text-centered mb-2 mr-1" key={todo.id}>{todo.value}</p>;
+              return (
+                <p
+                  className="box column is-5 ml-6 has-text-centered mb-2 mr-1"
+                  key={todo.id}
+                >
+                  {todo.value}
+                </p>
+              );
             })}
           </div>
         </div>
