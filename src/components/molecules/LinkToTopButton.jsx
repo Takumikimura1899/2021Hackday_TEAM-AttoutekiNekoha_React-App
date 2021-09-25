b91e1c20d45bd51aed20e712939cb46789ecfdb2
Takumikimura141899 <kimura141899@gmail.com>
2021-09-25 22:59:07 +0900

    [change] ローカルストレージクリアの挙動をcontentsだけ消すように変更

--- src/components/molecules/LinkToTopButton.jsx
+++ src/components/molecules/LinkToTopButton.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const LinkToTopButton = () => {
   const onClick = () => {
-    localStorage.clear();
+    localStorage.removeItem('contents');
   };
   return (
     <>
