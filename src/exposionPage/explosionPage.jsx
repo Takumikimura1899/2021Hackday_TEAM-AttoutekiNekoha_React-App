cb202e726e6b68b11484b0abb9df082a67252be0
Takumikimura141899 <kimura141899@gmail.com>
2021-09-26 03:21:55 +0900

    [clean] コメントアウト等削除

--- src/exposionPage/explosionPage.jsx
+++ src/exposionPage/explosionPage.jsx
@@ -1,26 +1,9 @@
-import { useState, useEffect } from 'react';
 import GifPlayer from 'react-gif-player';
 import recipeGif from '../static/recipeApp.gif';
-import { Link } from 'react-router-dom';
 import './explosion.css';
 import LinkToTopButton from '../components/molecules/LinkToTopButton';
 
 export const ExplosionPage = () => {
-  // const [localStorageContents, setLocalStorageContents] = useState(
-  //   JSON.parse(localStorage.getItem('contents')),
-  // );
-
-  // useEffect(() => {
-  //   if (localStorage.getItem('contents')) {
-  //     const getLocalStorageContents = localStorage.getItem('contents');
-  //     setLocalStorageContents(JSON.parse(getLocalStorageContents));
-  //   }
-  // }, []);
-
-  // console.log(localStorageContents);
-  // const text =
-  //   localStorageContents[Math.floor(Math.random() * localStorageContents.length)];
-
   const newArray = [];
   const getLocalStorageContents = JSON.parse(localStorage.getItem('contents'));
 
@@ -44,16 +27,6 @@ export const ExplosionPage = () => {
   return (
     <>
       <div className="main">
-        {/* <div className="explosion">
-          <GifPlayer gif={recipeGif} autoplay />
-          <p className="text1">{localStorageContents[0].value}</p>
-          <p className="text2">{localStorageContents[1].value}</p>
-          <p className="text3">{localStorageContents[2].value}</p>
-          <p className="text4">{localStorageContents[3].value}</p>
-        </div>
-        <div className="has-text-centered">
-          <LinkToTopButton />
-        </div> */}
         <div className="explosion">
           <GifPlayer gif={recipeGif} autoplay />
           {shuffleArray.map((todo, index) => {
@@ -65,9 +38,7 @@ export const ExplosionPage = () => {
           })}
         </div>
         <div className="has-text-centered">
-          <button className="button is-warning">
-            <Link to="/">TopPageに戻る</Link>
-          </button>
+          <LinkToTopButton />
         </div>
       </div>
     </>
